6f5ac7983376dacad14e45084ec25a934ebde3b2
Steven Lehrburger <lehrburger@gmail.com>
2019-02-24 14:12:10 -0500

    Initial working version of server, cleanup and testing next

--- server.py
+++ server.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
+import json
 import SimpleHTTPServer
 import SocketServer
 
 class FrameHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     frame = 1
+    last = 28602
     
     def do_GET(self):
-        print self.path
+        if self.path == '/increment.json':
+            FrameHandler.frame = FrameHandler.frame + 1
+            if FrameHandler.frame >= FrameHandler.last:
+                self.send_error(404, 'Frame {} Not Found'.format(FrameHandler.frame))
+            else:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({'current': FrameHandler.frame,
+                                             'next'   : FrameHandler.frame + 1}))
+            return
         if self.path == '/':
             self.path = '/index.html'
-            print FrameHandler.frame
-        if self.path.startswith('/current.png'):
-            self.path = '/frames/{}.png'.format(FrameHandler.frame)
-        if self.path.startswith('/next.png'):
-            print FrameHandler.frame
-            self.path = '/frames/{}.png'.format(FrameHandler.frame + 1)
-            FrameHandler.frame = FrameHandler.frame + 1
-        print self.path
         return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-
 def main():
-    FrameHandler.frame = 28450  # TODO make this a command line arg
+    FrameHandler.frame = 28450  # TODO make this a command line arg, and get max number too
     Handler = FrameHandler
     httpd = SocketServer.TCPServer(("", 8000), Handler)
 
