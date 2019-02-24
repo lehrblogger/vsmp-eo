08e5e33dfaf6b98393f82efc61cbc4d470b556e6
Steven Lehrburger <lehrburger@gmail.com>
2019-02-24 16:28:59 -0500

    Add script arguments and logging to a file

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 import json
-import SimpleHTTPServer
+import logging
+from optparse import OptionParser
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
 
-class FrameHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class FrameHandler(SimpleHTTPRequestHandler):
     frame = 1
-    last = 28602
-    
+    last = 1
+    log_file = None
+
+    def log_message(self, format, *args):
+        if FrameHandler.log_file:
+            FrameHandler.log_file.write("%s - - [%s] %s\n" % (self.client_address[0],
+                                                              self.log_date_time_string(),
+                                                              format%args))
+        else:
+            SimpleHTTPRequestHandler.log_message(self, format, *args)
+
     def do_GET(self):
         if self.path == '/increment.json':
-            FrameHandler.frame = FrameHandler.frame + 1
             if FrameHandler.frame >= FrameHandler.last:
                 self.send_error(404, 'Frame {} Not Found'.format(FrameHandler.frame))
             else:
+                FrameHandler.frame = FrameHandler.frame + 1
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
@@ -21,14 +32,45 @@ class FrameHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             return
         if self.path == '/':
             self.path = '/index.html'
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        return SimpleHTTPRequestHandler.do_GET(self)
+
 
 def main():
-    FrameHandler.frame = 28450  # TODO make this a command line arg, and get max number too
-    Handler = FrameHandler
-    httpd = SocketServer.TCPServer(("", 8000), Handler)
+    usage =  'python server.py -f 1 -l 70000 -p 8000'
+    description = 'Turn your Electric Objects EO1 or EO2 into a Very Slow Movie Player.'
+    epilog = 'See also https://github.com/lehrblogger/vsmp-eo'
 
-    httpd.serve_forever()
+    parser = OptionParser(usage=usage, description=description, epilog=epilog)
+    parser.add_option('-f', '--first', dest='first',
+                      help='the number of the first frame to display',
+                      type='int', default=1)
+    parser.add_option('-l', '--last', dest='last',
+                      help='the number of the last available frame',
+                      type='int')
+    parser.add_option('-p', '--port', dest='port',
+                      help='the port on which to run the server',
+                      type='int', default=8000)
+    parser.add_option('-o', '--output', dest='log_file',
+                      help='the file to which to write the log output',
+                      type='str', default=None)
+    (options, args) = parser.parse_args()
+    options_ok = True
 
-if __name__ == "__main__":
+    if not options.last:
+        logging.error('Please specify the last available frame with either "-l" or --last"')
+        options_ok = False
+
+    if not options_ok:
+        logging.error('See "python server.py --help" for all options.')
+    else:
+        FrameHandler.frame = options.first
+        FrameHandler.last = options.last
+        if options.log_file:
+            FrameHandler.log_file = open(options.log_file, 'a', 0)
+        Handler = FrameHandler
+        httpd = SocketServer.TCPServer(('', options.port), Handler)
+        httpd.serve_forever()
+
+
+if __name__ == '__main__':
     main()
